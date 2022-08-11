962957eeee5d1ea2d12ee3fbca9f03b7b1770403
Kyle Coleman <kcoleman20@cnm.edu>
2022-08-11 16:32:58 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+
+  public String findNameByLetter() {
+    found
+
+    if (search.charAt(0) == A) {
+    return found;
+  }
+}
